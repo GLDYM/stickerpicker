456ea1069e9f40394174ea17f4cb71b1d4367158
Polaris_Light <995905922@qq.com>
2024-06-17 16:27:52 +0800

    del build

--- build/lib/sticker/version.py
+++
@@ -1,6 +0,0 @@
-# Generated in setup.py
-
-git_tag = None
-git_revision = '07e550d3'
-version = '0.1.0+dev.07e550d3'
-linkified_version = '0.1.0+dev.[07e550d3](https://github.com/maunium/stickerpicker/commit/07e550d36ccf4e78e9c1cb128325361c57e1993f)'
